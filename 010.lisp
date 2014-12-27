b9d4dba67e5f3a34fe7b18bfdf86764b8a6133b1
shadowfiles <zhangtiff@gmail.com>
2014-12-26 23:00:37 -0500

    Changed code to focus on getting sum.

--- 010.lisp
+++ 010.lisp
@@ -1,16 +1,13 @@
 (defun sum-primes-below (max)
 	(let 
-		((primes (primes-below max)))
-	(loop for i from 2 to max
-		when (aref primes i)
-		sum i)))
-
-(defun primes-below (max)
-	(let 
 		((primes (make-array (+ max 1) 
-				:initial-element t)))
-			(loop for num from 2 to (sqrt max)
+				:initial-element t))
+		 (sum 0))
+			(loop for num from 2 to max
 				do (if (aref primes num)
-					(loop for i from (* 2 num) to max by num
-						do (setf (aref primes i) nil))))
-		primes))
+					(progn 
+						(if (<= num (sqrt max)) 
+							(loop for i from (* 2 num) to max by num
+							do (setf (aref primes i) nil)))
+						(setq sum (+ sum num)))))
+		sum))
