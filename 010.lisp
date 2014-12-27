1f80a10f301c6b7ac2c9d2ebf8f7951c75cd5893
shadowfiles <zhangtiff@gmail.com>
2014-12-26 23:29:49 -0500

    Small optimizations.

--- 010.lisp
+++ 010.lisp
@@ -1,13 +1,18 @@
 (defun sum-primes-below (max)
 	(let 
 		((primes (make-array (+ max 1) 
-				:initial-element t))
-		 (sum 0))
-			(loop for num from 2 to max
-				do (if (aref primes num)
+				:initial-element 1
+				:element-type '(mod 2)))A
+		 (sum 2))
+		(loop for num from 3 to (floor (sqrt max))
+				do (if (and 
+						(= (aref primes num) 1)
+						(= 0 (mod num 2)))
 					(progn 
-						(if (<= num (sqrt max)) 
-							(loop for i from (* 2 num) to max by num
-							do (setf (aref primes i) nil)))
+						(loop for i from (* 2 num) to max by num
+							do (setf (aref primes i) 0))
 						(setq sum (+ sum num)))))
+		(loop for num from (+ (floor (sqrt max)) 1) to max 
+				do (if (= (aref primes num) 1)
+					(setq sum (+ sum num))))
 		sum))
