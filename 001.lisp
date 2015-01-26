4a74680742a5dad841edeb899158edcc997dbffe
shadowfiles <zhangtiff@gmail.com>
2015-01-26 17:11:26 -0500

    Edited solution to use STDIN and STDOUT for user in Hackerrank.

--- 001.lisp
+++ 001.lisp
@@ -1,9 +1,14 @@
 (defun fizzbuzz (mulA mulB max)
-	(setq max (- max 1))
-	(setq mulC (* mulA mulB))
-	(setq a (floor max mulA))
-	(setq b (floor max mulB))
-	(setq c (floor max mulC))
-	(-(+ (* mulA (floor (* a (+ a 1)) 2))
+  (setq max (- max 1))
+	(let 
+     ((a (floor max mulA))
+      (b (floor max mulB))
+      (c (floor max (* mulA mulB))))
+     (-(+ (* mulA (floor (* a (+ a 1)) 2))
 		(* mulB (floor (* b (+ b 1)) 2)))
-		(* mulC (floor (* c (+ c 1)) 2))))
+		(* (* mulA mulB) (floor (* c (+ c 1)) 2)))))
+
+(loop while (listen) 
+   do(let 
+      (result (fizzbuzz 3 5 (read))) 
+      (if (> result 0) (print result))))
